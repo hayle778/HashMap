984a552b05ec3df52696a41edf916fa661b45c00
yourUsername <yourEmail@domain.com>
2020-01-28 23:09:19 -0500

    ff

---
+++ src/ClassMain.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+
+public class ClassMain {
+public static void main(String[]args){
+
+
+            int a =10;
+            int b = 3;
+            int c = 88;
+
+
+            HashMap <String, Integer> happy = new HashMap();
+
+            happy.put("a", 10);
+            happy.put("b", 3);
+            happy.put("c", 88);
+
+
+            System.out.println(happy);
+
+
+
+
+
+
+        }
+    }
+
+
+
+
